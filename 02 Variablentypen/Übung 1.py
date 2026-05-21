631102964ea9376eba38c0d7f3bd8582f2fcc4fa
Eric <cheesecaketv53@gmail.com>
2026-05-21 21:40:43 +0200

    Lösungen

---
+++ 02 Variablentypen/Übung 1.py
@@ -0,0 +1,16 @@
+
+# 1.
+eingabe = int(input())
+print(eingabe * 2)
+
+# 2.
+eingabe2 = int(input())
+print(eingabe * eingabe2)
+
+# 3.
+eingabe = float(input())
+eingabe2 = float(input())
+
+print(eingabe * eingabe2)
+
+
---
+++ 02 Variablentypen/Übung 2.py
@@ -0,0 +1,17 @@
+
+# int zu bool
+# True für alles außer 0
+
+# bool zu int
+# False -> 0, True -> 1
+
+# str zu bool
+# True wenn String nicht leer
+# Leer = ""
+
+# bool zu str
+# Einfach das Wort des bools
+
+# None zu bool
+# False
+
