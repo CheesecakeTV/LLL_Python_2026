eb6c671ab8e1c2c4f45aa5e180f5817b8e64e3b5
Eric <cheesecaketv53@gmail.com>
2026-05-28 20:28:37 +0200

    bla

---
+++ 03 Bedingungen und Verzweigungen/Mitschrift.py
@@ -0,0 +1,26 @@
+
+x = False
+y = True
+
+z = x and y
+a = x or y
+b = not x
+c = x and (y or z)
+
+x = 16
+y = 20
+
+a = x == y
+b = x != y
+c = x > y
+d = x >= y
+e = x < y
+
+teilbar = x % y == 0
+
+assert x % 3 == 0, "x muss durch 3 teilbar sein"
+assert abs(x) <= 10
+
+f = x is None
+
+
