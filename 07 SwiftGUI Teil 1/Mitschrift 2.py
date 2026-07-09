c39e0909d714d0d3be9a1ae66a554c011b9a9410
Eric <cheesecaketv53@gmail.com>
2026-07-09 21:51:55 +0200

    M7

--- 07 SwiftGUI Teil 1/Mitschrift 2.py
+++ 07 SwiftGUI Teil 1/Mitschrift 2.py
@@ -2,15 +2,32 @@ import SwiftGUI as sg
 
 sg.Themes.FourColors.DarkGold()
 
+def zeile(key: str) -> list[sg.BaseElement]:
+    def knopf():      # e, v, elem: sg.BaseElement):
+        # elem.value = "Gedrückt"
+        meinInput.value = ""
+
+    return [
+        #sg.Input(key="Input"),
+        meinInput := sg.Input(key=key, default_event=True, width=50),
+        sg.Spacer(width=5),
+        sg.Button(" x ", key_function=knopf)
+    ]
+
 layout = [
     [
-        sg.Input(),
-        sg.Button(" x ", key="Clear")
+        *zeile("Name"),
+        *zeile("Alter"),
+        *zeile("Nachname"),
     ]
 ]
 
 w = sg.Window(layout)
 
 for e,v in w:
-    ...
+    print(e, v)
+
+    # if e == "Clear":
+    #     #v["Input"] = ""
+    #     meinInput.value = ""
 
