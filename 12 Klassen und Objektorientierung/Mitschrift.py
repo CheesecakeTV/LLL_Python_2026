0779def6bc96cac510661323bdd24fccc4060bfb
Eric <cheesecaketv53@gmail.com>
2026-08-13 21:40:52 +0200

    M12

--- 12 Klassen und Objektorientierung/Mitschrift.py
+++ 12 Klassen und Objektorientierung/Mitschrift.py
@@ -8,6 +8,22 @@ class Haus:
         self.tueren = 7
         self._liste = [1,2,3]
 
+    def __eq__(self, other: "Haus") -> bool:
+        return self.wande == other.wande
+
+    def __str__(self) -> str:
+        return f"<Haus-Objekt {self.wande=}, {self.tueren=}>"
+
+    def __bool__(self) -> bool:
+        return self.tueren > 0
+
+    def __repr__(self) -> str:
+        #return str(self)
+        return f"<Haus-Objekt {id(self)} {self.wande=}, {self.tueren=}>"
+
+    def __hash__(self) -> int:
+        return hash((self.wande, self.tueren))
+
     def print_info(self):
         print(self.wande, self.tueren)
 
@@ -16,11 +32,26 @@ class Haus:
         return int(tueren / 4)
 
 haus1 = Haus(16)
-haus1.print_info()
-haus1.wande = 15
-haus1.print_info()
+haus2 = Haus(16)
 
-print(haus1.berechne_wande(15))
+if haus1:
+    ...
 
-print(dir(haus1))
+meine_liste = [haus1, haus2]
+#meine_liste = ["1",2,3]
+print(meine_liste)
+
+print(haus1 == haus2) # haus1.__eq__(haus2)
+print(haus1 is haus2)
+
+# haus1.print_info()
+# haus1.wande = 15
+# haus1.print_info()
+
+#print(haus1.berechne_wande(15))
+
+#print(dir(haus1))
+
+mein_dict = {haus1: haus2, haus2: haus1}
+print(mein_dict)
 
